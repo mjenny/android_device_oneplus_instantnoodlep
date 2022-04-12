b4771385819b797efa9878debe1da4078f6bc0b0
Manuel Jenny <git@manueljenny.ch>
2022-04-12 09:07:31 +0200

    Initial calyx makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_instantnoodlep.mk
     $(LOCAL_DIR)/lineage_instantnoodlep.mk
 
 COMMON_LUNCH_CHOICES := \
+    calyx_instantnoodlep-user \
+    calyx_instantnoodlep-userdebug \
+    calyx_instantnoodlep-eng \
     lineage_instantnoodlep-user \
     lineage_instantnoodlep-userdebug \
     lineage_instantnoodlep-eng

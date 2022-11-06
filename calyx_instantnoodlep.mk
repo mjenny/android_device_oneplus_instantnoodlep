97d13ebeee1792e9a0dedcd7ceaf077121d17534
Manuel Jenny <git@manueljenny.ch>
2022-11-06 14:57:30 +0100

    Initial calyx makefiles

---
+++ calyx_instantnoodlep.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from instantnoodlep device
+$(call inherit-product, device/oneplus/instantnoodlep/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_instantnoodlep
+PRODUCT_DEVICE := instantnoodlep
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := IN2025
+
+PRODUCT_SYSTEM_NAME := OnePlus8Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus8Pro
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus8Pro-user 12 RKQ1.211119.001 Q.202210170945 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus8Pro_EEA/OnePlus8Pro:12/RKQ1.211119.001/Q.GDPR.202210170945:user/release-keys
